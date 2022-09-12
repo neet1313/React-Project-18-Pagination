ea5635a526763d9f2a4d46e1d609fdbcf340a0df
Prabhneet Singh <prabhneet13@gmail.com>
2022-09-12 18:11:50 +0530

    adding next and prev buttons

--- src/App.js
+++ src/App.js
@@ -10,9 +10,27 @@ function App() {
     if (loading) return;
 
     setFollowers(data[page]);
-  }, [loading, page]);
+  }, [loading, page, data]);
 
   const handlePage = (index) => { setPage(index) }
+
+  const clickHandler = (payload) => {
+    if (payload === "next") {
+      setPage(() => {
+        let nextPage = page + 1;
+        (nextPage > data.length - 1) && (nextPage = 0);
+
+        return nextPage;
+      });
+    } else {
+      setPage(() => {
+        let prevPage = page - 1;
+        (prevPage < 0) && (prevPage = data.length - 1);
+
+        return prevPage;
+      });
+    }
+  }
 
   return <main>
     <div className='section-title'>
@@ -23,11 +41,15 @@ function App() {
       <div className='container'>
         {followers.map(follower => <Follower key={follower.id} {...follower} />)}
       </div>
+
       {!loading && <div className='btn-container'>
+        <button type='button' className='prev-btn' onClick={() => clickHandler("prev")}>prev</button>
         {data.map((item, index) => {
           return <button key={index} className={`page-btn ${page === index && 'active-btn'}`} onClick={() => { handlePage(index) }}>{index + 1}</button>
         })}
+        <button type='button' className='next-btn' onClick={() => clickHandler("next")} >next</button>
       </div>}
+
     </section>
   </main>
 }
